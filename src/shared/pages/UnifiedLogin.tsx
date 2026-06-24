6da2a454197cd1065f00bbeee7e34937391ff80b
Ficium Admin <kishan.jeebun@ficium.net>
2026-06-24 11:53:24 +0400

    feat: import ficiumAuthFetch in UnifiedLogin

--- src/shared/pages/UnifiedLogin.tsx
+++ src/shared/pages/UnifiedLogin.tsx
@@ -20,7 +20,7 @@
 import { useState, useEffect } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import { Eye, EyeOff, Shield, ArrowRight, Building2, Zap } from 'lucide-react'
-import { signIn as ficiumSignIn, getTokenPayload, hasSession, signOut as ficiumSignOut } from '../lib/ficiumAuth'
+import { ficiumAuthFetch, signIn as ficiumSignIn, getTokenPayload, hasSession, signOut as ficiumSignOut } from '../lib/ficiumAuth'
 import FiciumLogo from '../ui/FiciumLogo'
 import { GradText } from '../ui/dashboard/Hero'
 
